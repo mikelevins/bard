361bedb3a346629be8f0e1e5f5dd25ae0130a505
mikel evins <mevins@mac.com>
2010-11-15 17:11:17 -0700

    Implemented the simplest cases of reductions

--- implementations/ghc/vm/src/Environment.hs
+++ implementations/ghc/vm/src/Environment.hs
@@ -12,8 +12,22 @@ import Expressions
 data Environment = Env (M.Map Expression Expression) deriving (Show, Eq)
 
 -------------------------------------------------
+-- constructors
+-------------------------------------------------
+
+extendEnv :: Environment -> Expression -> Expression -> Environment
+extendEnv (Env env) nm val = Env (M.insert nm val env)
+
+lookupName (Env env) nm = env M.! nm
+
+-------------------------------------------------
+-- accessors
+-------------------------------------------------
+
+-------------------------------------------------
 -- standard environment
 -------------------------------------------------
 
 nullEnvironment = Env M.empty
-standardEnvironment = nullEnvironment
+standardEnvironment = (extendEnv nullEnvironment (ExpName "nothing") ExpNothing)
+
--- implementations/ghc/vm/src/bard.hs
+++ implementations/ghc/vm/src/bard.hs
@@ -1,6 +1,9 @@
 module Main
   where
 
+import Numeric (readInt)
+import System (getArgs)
+
 import qualified Data.Map as M
 
 import Expressions
@@ -12,21 +15,49 @@ import Environment
 
 type VM = (Expression, Environment)
 
-transition :: VM -> VM
-transition (ExpNothing, nullEnvironment) = (ExpNothing, nullEnvironment)
+isNormalForm :: Expression -> Environment -> Bool
+isNormalForm ExpNothing _ = True
+isNormalForm (ExpInt int) _ = True
+isNormalForm (ExpFloat fl) _ = True
+isNormalForm (ExpChar ch) _ = True
+isNormalForm (ExpName nm) _ = False
+isNormalForm exp env = True
+
+reduce :: VM -> VM
+reduce (ExpNothing, env) = (ExpNothing, env)
+reduce ((ExpInt int), env) = ((ExpInt int), env)
+reduce ((ExpFloat fl), env) = ((ExpFloat fl), env)
+reduce ((ExpChar ch), env) = ((ExpChar ch), env)
+reduce ((ExpName nm), env) = ((lookupName env (ExpName nm)), env)
 
 runVM :: VM -> VM
 runVM (exp, env) =
-    let (exp', env') = transition (exp, env)
-    in if (exp', env') == (exp, env)
-       then (exp',env')
-       else runVM (exp', env')
-    
-    
+    if isNormalForm exp env
+       then (exp, env)
+       else
+           runVM (reduce (exp, env))
+
+-------------------------------------------------
+-- test programs
+-------------------------------------------------
+
+testPrograms = [
+ ExpNothing,
+ (ExpInt 23),
+ (ExpFloat 12.34),
+ (ExpChar 'B'),
+ (ExpName "nothing")]
+
 -------------------------------------------------
 -- main program
 -------------------------------------------------
 
 main = do
-       putStrLn "Bard VM v 1.0"
-       putStrLn (show (runVM (ExpNothing, nullEnvironment)))
+  args <- getArgs
+  let index = read (args!!0)
+      prog = testPrograms!!index
+      (result, _) = (runVM (prog, standardEnvironment))
+  putStrLn "Bard VM v 1.0"
+  putStrLn ("in: "++(show prog))
+  putStrLn ("out: "++(show result))
+
