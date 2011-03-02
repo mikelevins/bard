27ed38dc28cd60bd6430e35e6f2de075e8567620
mikel <mikel@latheya.(none)>
2011-03-02 13:33:57 -0700

    wrote parsers

---
+++ src/interpreter/src/BardValue.hs
@@ -0,0 +1,75 @@
+module BardValue
+    where
+
+import Data.List as L
+import Data.Map as M
+import Data.Sequence as S
+
+-------------------------------------------------
+-- Values
+-------------------------------------------------
+
+type Name = String
+
+data BardValue = BardUndefined
+           | BardNothing
+           | BardBoolean Bool
+           | BardInteger Integer
+           | BardFloat Double
+           | BardCharacter Char
+           | BardText String
+           | BardName Name
+           | BardSequence (S.Seq BardValue)
+           | BardMap (M.Map BardValue BardValue)
+             deriving (Eq, Ord)
+
+-- constructors
+
+nothing = BardNothing
+bool b = BardBoolean b
+int n = BardInteger n
+float f = BardFloat f
+name nm = BardName nm
+
+emptyMap = BardMap M.empty
+
+map :: [(BardValue,BardValue)] -> BardValue
+map [] = BardMap M.empty
+map vals = BardMap (M.fromList vals)
+
+emptySequence = BardSequence S.empty
+
+sequence :: [BardValue] -> BardValue
+sequence [] = BardSequence S.empty
+sequence vals = BardSequence (S.fromList vals)
+
+append :: BardValue -> BardValue -> BardValue
+append (BardSequence s1) (BardSequence s2) = (BardSequence (s1 >< s2))
+appedn _ _ = BardUndefined
+
+-- accessors
+
+first (BardSequence s) = S.index s 0
+second (BardSequence s) = S.index s 1
+third (BardSequence s) = S.index s 2
+
+get (BardMap m) k = M.lookup k m
+
+-- printers
+
+instance Show BardValue where
+    show BardUndefined = "undefined"
+    show BardNothing = "nothing"
+    show (BardBoolean True) = "true"
+    show (BardBoolean False) = "false"
+    show (BardInteger n) = (show n)
+    show (BardFloat f) = (show f)
+    show (BardCharacter ch) = ['\\',ch]
+    show (BardName n) =  n
+    show (BardMap m) = "{" ++ (expand m) ++ "}"
+        where expand m = concat (L.intersperse " " (Prelude.map showPair (M.assocs m)))
+              showPair (k, v) = (show k) ++ " " ++ (show v)
+    show (BardSequence s) = "(" ++ (expand s) ++ ")"
+        where expand s = concat (L.intersperse " " (Prelude.map showNth (Prelude.take (S.length s) [0 ..])))
+              showNth i = (show (index s i))
+
---
+++ src/interpreter/src/bard.hs
@@ -0,0 +1,78 @@
+module Main
+  where
+
+import Control.Monad
+import System
+import Text.ParserCombinators.Parsec hiding (spaces)
+
+import BardValue
+
+punctuation :: Parser Char
+punctuation = oneOf "!#$%&|*+_?:<=>?@^_~" 
+
+spaces :: Parser ()
+spaces = skipMany1 space
+
+parseString :: Parser BardValue
+parseString = do char '"'
+                 x <- many (noneOf "\"")
+                 char '"'
+                 return (BardText x)
+                 
+parseName :: Parser BardValue
+parseName = do first <- letter <|> punctuation
+               rest <- many (letter <|> digit <|> punctuation)
+               let nm = first:rest
+               case nm of
+                 "undefined" -> return BardUndefined
+                 "nothing" -> return BardNothing
+                 "true" -> return (BardBoolean True)
+                 "false" -> return (BardBoolean False)
+                 _ -> return (BardName nm)
+                 
+parseInteger :: Parser BardValue
+parseInteger = liftM (BardInteger . read) $ many1 digit
+
+parseSequence :: Parser BardValue
+parseSequence = liftM BardValue.sequence $ sepBy parseExpr spaces 
+
+parseQuoted :: Parser BardValue
+parseQuoted = do
+  char '\''
+  x <- parseExpr
+  return (BardValue.sequence [BardName "quote", x])
+
+parseExpr :: Parser BardValue
+parseExpr = parseName
+        <|> parseString
+        <|> parseInteger
+        <|> parseQuoted
+        <|> do char '('
+               x <- parseSequence
+               char ')'
+               return x
+        <|> do char '['
+               x <- parseSequence
+               char ']'
+               let op = (BardName "sequence")
+               return (BardValue.append (BardValue.sequence [op]) x)
+
+readExpr :: String -> String
+readExpr input = case parse parseExpr "bard" input of
+  Left err -> "No match:" ++ show err
+  Right val -> "Found value: " ++ (show val)
+
+-------------------------------------------------
+-- 
+-------------------------------------------------
+
+-------------------------------------------------
+-- main program
+-------------------------------------------------
+
+main = do
+       putStrLn "Bard v 1.0"
+       putStrLn ""
+       args <- getArgs
+       putStrLn (readExpr (args !! 0))
+
