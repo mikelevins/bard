08df36761c41f42385d9d71d20f763090fbddb3c
mikel evins <mevins@me.com>
2011-03-06 17:01:48 -0600

    Implemented evaluating unqualified variables in the current module

--- src/interpreter/src/Eval.hs
+++ src/interpreter/src/Eval.hs
@@ -17,7 +17,18 @@ eval val@(BardFloat _) _ = do return  val
 eval val@(BardCharacter _) _ = do return  val
 eval val@(BardText _) _ = do return  val
 
-eval (BardName (vname,mname)) mmgr = do
-	v <- getVar mmgr mname vname
-	get v
+eval var@(BardName (vname,mname)) mmgr = do
+	if (mname == "bard.keyword")
+		then return var
+		else if (mname == "")
+			then evalUnqualifiedVarName vname mmgr
+			else evalQualifiedVarName vname mname mmgr
 
+evalUnqualifiedVarName vname mmgr = do
+	currm <- getCurrentModule mmgr
+	var <- getVar mmgr currm vname
+	get var
+	
+evalQualifiedVarName vname mname mmgr = do
+	var <- getVar mmgr mname vname
+	get var
