efea26d5b62bb6715a73d387b9eb2db4c1f440ed
mikel evins <mevins@me.com>
2011-09-08 12:36:38 -0500

    added some instruction-handling code

--- gambit/vm/scm/bardvm.scm
+++ gambit/vm/scm/bardvm.scm
@@ -8,6 +8,10 @@
 ;;;;
 ;;;; ***********************************************************************
 
+;;; ---------------------------------------------------------------------
+;;; the VM
+;;; ---------------------------------------------------------------------
+
 (define-type bardvm
   id: 6336A58B-0E6A-4EF1-BFAD-0DB264DFEE42
   constructor: %make-bardvm
@@ -36,11 +40,77 @@
                            prims argcount instruction halt)))
     (%make-bardvm regvector)))
 
-(define (vm%reg vm reg)
+(define (vm:%reg vm reg)
   (vector-ref (vm:%get-registers vm) reg)
   vm)
 
-(define (vm%reg-set! vm reg val)
+(define (vm:%reg-set! vm reg val)
   (vector-set! (vm:%get-registers vm) reg val)
   vm)
 
+(define (vm:error msg)
+  (error msg))
+
+;;; ---------------------------------------------------------------------
+;;; PC operations
+;;; ---------------------------------------------------------------------
+
+(define (vm:incpc vm)
+  (vm:%reg-set! vm reg:PC (+ 1 (vm:%reg vm reg:PC))))
+
+;;; ---------------------------------------------------------------------
+;;; stack operations
+;;; ---------------------------------------------------------------------
+
+(define (vm:%ensure-stack-capacity vm n)
+  (if (not (< (+ n (vm:%reg vm reg:STACK-DEPTH))
+              (vm:%reg vm reg:STACK-LIMIT)))
+      (vm:error "stack full")))
+
+(define (vm:%check-stack-empty vm)
+  (if (< (vm:%reg vm reg:STACK-DEPTH) 1)
+      (vm:error "stack empty")))
+
+(define (vm:push vm x)
+  (vm:%ensure-stack-capacity vm 1)
+  (vm:%reg-set! vm reg:STACK-DEPTH (+ 1 (vm:%reg vm reg:STACK-DEPTH)))
+  (vm:%reg-set! vm reg:STACK (cons x (vm:%reg vm reg:STACK))))
+
+(define (vm:pop vm)
+  (vm:%check-stack-empty vm)
+  (vm:%reg-set! vm reg:STACK-DEPTH (- (vm:%reg vm reg:STACK-DEPTH) 1))
+  (let ((val (car (vm:%reg vm reg:STACK))))
+    (vm:%reg-set! vm reg:STACK (cdr (vm:%reg vm reg:STACK)))
+    val))
+
+;;; ---------------------------------------------------------------------
+;;; instructions
+;;; ---------------------------------------------------------------------
+
+(define (vm:%make-instruction opcode . args)
+  (vm:%check-argcount opcode args)
+  (apply vector (cons opcode args)))
+
+(define (instruction:%opcode instr)
+  (vector-ref instr 0))
+
+(define (instruction:%arg instr index)
+  (vector-ref instr index))
+
+;;; ---------------------------------------------------------------------
+;;; instruction execution
+;;; ---------------------------------------------------------------------
+
+(define (vm:%current-instruction vm)
+  (vector-ref (vm:%reg vm reg:CODE)
+              (vm:%reg vm reg:PC)))
+
+;;; argcount: 0
+(define (vm:noop vm)
+  (vm:incpc vm)
+  vm)
+
+;;; argcount: 1
+(define (vm:const vm)
+  (vm:push vm (instruction:%arg (vm:%current-instruction vm) 1))
+  vm)
