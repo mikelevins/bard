d5e914bf678de7c96ddab2ae0d82d884a3f04bb4
mikel evins <mevins@me.com>
2012-03-19 14:18:04 -0500

    reworked environments; added support for special forms

--- interpreter/bard.scm
+++ interpreter/bard.scm
@@ -25,6 +25,17 @@
 (define (not-yet-implemented . args)
   (error "Not yet implemented" args))
 
+(define (->table . k/v-plist)
+  (let ((tbl (make-table test: eq?)))
+    (let loop ((kvs k/v-plist))
+      (if (null? kvs)
+          tbl
+          (if (null? (cdr kvs))
+              (error "odd number of inits in make-table" k/v-plist)
+              (begin
+                (table-set! tbl (car kvs)(cadr kvs))
+                (loop (cddr kvs))))))))
+
 ;;;---------------------------------------------------------------------
 ;;; bard cells
 ;;;---------------------------------------------------------------------
@@ -100,22 +111,30 @@
   (assq var env))
 
 (define toplevel-environment #f)
+(define set-toplevel-environment! #f)
 
 (let ((_env #f))
   (set! toplevel-environment
         (lambda ()
           (if _env
-              _env
+              (bard:get-cell _env)
               (begin
                 (set! _env (bard:make-cell (make-default-environment)))
-                _env)))))
+                (bard:get-cell _env)))))
+  (set! set-toplevel-environment!
+        (lambda (env)
+          (if _env
+              (bard:put-cell! _env env)
+              (begin
+                (set! _env (bard:make-cell env))
+                (bard:get-cell _env))))))
 
 (define (define-variable! var val)
   (bard:put-cell! (toplevel-environment)
-                  (extend-environment (bard:get-cell (toplevel-environment)))))
+                  (extend-environment (toplevel-environment))))
 
 (define (lookup-variable-value var env)
-  (let* ((top-env (bard:get-cell (toplevel-environment)))
+  (let* ((top-env (toplevel-environment))
          (entry (find-entry env var)))
     (if entry
         (cdr entry)
@@ -123,6 +142,29 @@
           (if entry
               (cdr entry)
               (undefined))))))
+
+;;;---------------------------------------------------------------------
+;;; special forms
+;;;---------------------------------------------------------------------
+
+(define (bard:define-variable var val)
+  (define-variable! var val))
+
+(define $special-forms-table
+  (->table
+   'define bard:define-variable))
+
+(define (special-form? expr)
+  (and (list? expr)
+       (table-ref $special-forms-table (car expr))
+       #t))
+
+;;;---------------------------------------------------------------------
+;;; macros
+;;;---------------------------------------------------------------------
+
+(define (macro? expr)
+  #f)
 
 ;;;---------------------------------------------------------------------
 ;;; evaluator
@@ -136,12 +178,37 @@
       (string? x)
       (keyword? x)))
 
+(define (bard:eval-special-form expr env)
+  (let ((op (table-ref $special-forms-table (car expr))))
+    (apply op (cdr expr))))
+
+(define (bard:eval-macro expr env)
+  #f)
+
+(define (bard:apply op args)
+  (apply op args))
+
+(define (bard:eval-application expr env)
+  (let ((op (bard:eval (car expr) env))
+        (args (map (lambda (x)(bard:eval x env))
+                   (cdr expr))))
+    (bard:apply op args)))
+
+(define (bard:eval-list expr env)
+  (cond
+   ((special-form? expr)(bard:eval-special-form expr env))
+   ((macro? expr)(bard:eval-macro expr env))
+   (else (bard:eval-application expr env))))
+
 (define (bard:eval expr env)
   (cond
    ((self-evaluating? expr) exp)
    ((symbol? expr)(lookup-variable-value expr env))
+   ((list? expr)(bard:eval-list expr env))
    (else (error "unrecognized expression type" expr))))
 
-;;; (bard:eval '+ (bard:get-cell (toplevel-environment)))
+;;; (bard:eval '+ (toplevel-environment))
+;;; (bard:eval '- (toplevel-environment))
+;;; (bard:eval 'x (toplevel-environment))
 
 
