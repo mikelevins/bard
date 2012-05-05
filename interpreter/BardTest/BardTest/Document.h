cc39306da8c3ef1c0873de186173079099f08750
mikel evins <mevins@me.com>
2012-05-05 10:53:27 -0500

    added an XCode project for testing the Bard library

---
+++ interpreter/BardTest/BardTest/Document.h
@@ -0,0 +1,13 @@
+//
+//  Document.h
+//  BardTest
+//
+//  Created by mikel evins on 5/5/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface Document : NSDocument
+
+@end
