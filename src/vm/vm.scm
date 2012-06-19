64a3b6ede6e3212aef42e582ddff28e355bde0de
mikel evins <mevins@me.com>
2012-06-19 09:35:06 -0500

    vm executing basic instructions and optionally printing each step

--- src/vm/vm.scm
+++ src/vm/vm.scm
@@ -11,8 +11,19 @@
 
 
 ;;; ---------------------------------------------------------------------
+;;; utilities
+;;; ---------------------------------------------------------------------
+
+(define (false? x)(not x))
+(define (true? x)(not (false? x)))
+
+;;; ---------------------------------------------------------------------
 ;;; global variables
 ;;; ---------------------------------------------------------------------
+
+(define (vm:make-globals)(make-table test: eq?))
+
+(define $absent (cons 'absent '()))
 
 (define (vm:gref vm var)
   (table-ref (vm:globals vm) var $absent))
@@ -46,10 +57,7 @@
 ;;; ---------------------------------------------------------------------
 
 (define (opcode instr)(vector-ref instr 0))
-(define (vm:frame-index instr)(vector-ref instr 1))
-(define (vm:var-index instr)(vector-ref instr 2))
-(define (vm:gvarname instr)(vector-ref instr 1))
-(define (vm:arg1 instr)(vector-ref instr 1))
+(define (instr:arg instr n)(vector-ref instr n))
 
 ;;; ---------------------------------------------------------------------
 ;;; functions
@@ -66,6 +74,10 @@
 
 (define (vm:as-code args)
   (apply vector (map (lambda (a)(apply vector a)) args)))
+
+;;; ---------------------------------------------------------------------
+;;; return addresses
+;;; ---------------------------------------------------------------------
 
 ;;; ---------------------------------------------------------------------
 ;;; the vm
@@ -91,7 +103,7 @@
                  (stack '())
                  (argcount 0)
                  (instruction #f))
-  (%private-make-vm #f fun (vm:function-code fun) pc (make-table test: eqv?) env stack argcount instruction))
+  (%private-make-vm #f fun (vm:function-code fun) pc (vm:make-globals) env stack argcount instruction))
 
 (define (vm:push! vm val)
   (vm:set-stack! vm (cons val (vm:stack vm))))
@@ -102,8 +114,15 @@
 
 (define (vm:pop! vm)
   (let ((out (vm:top vm)))
-    (vm:set-stack (cdr (vm:stack vm)))
+    (vm:set-stack! vm (cdr (vm:stack vm)))
     out))
+
+(define (vm:swap! vm)
+  (let ((old-top (vm:pop vm))
+        (old-next (vm:pop vm)))
+    (vm:push! vm old-top)
+    (vm:push! vm old-next)
+    old-next))
 
 ;;; ---------------------------------------------------------------------
 ;;; vm operations
@@ -112,47 +131,231 @@
 (define $vm-operations (make-vector 255))
 (define $vm-operation-names (make-vector 255))
 
-(define-macro (defop nm num fn)
-  `(begin
-     (define ,nm ,num)
-     (vector-set! $vm-operations ,num ,fn)
-     (vector-set! $vm-operation-names ,num ,nm)))
-
 (define (vm:op n)(vector-ref $vm-operations n))
 (define (vm:opname n)(vector-ref $vm-operation-names n))
 
-(defop LVAR  1 
-  (lambda (vm)
-    (vm:push! vm (vm:lvar-ref (vm:env vm)
-                              (vm:frame-index (vm:instruction vm))
-                              (vm:var-index (vm:instruction vm))))))
+(define-macro (operation proto body)
+  (let* ((nm (car proto))
+         (args (cdr proto))
+         (bindings (let loop ((args args)
+                              (i 0)
+                              (blist '()))
+                     (if (null? args)
+                         (reverse blist)
+                         (loop (cdr args)
+                               (+ i 1)
+                               (cons `(,(car args) (instr:arg (vm:instruction vm) (+ 1 ,i)))
+                                     blist)))))
+         (fn (gensym)))
+    `(lambda (vm)(let ,bindings ,@body))))
 
-(defop LSET  2
-  (lambda (vm)
-    (vm:lvar-set! vm 
-                  (vm:frame-index (vm:instruction vm))
-                  (vm:var-index (vm:instruction vm))
-                  (vm:top vm))))
+(define-macro (defop num proto . body)
+  `(begin
+     (define ,(car proto) ,num)
+     (vector-set! $vm-operations ,num (operation ,proto ,body))
+     (vector-set! $vm-operation-names ,num ',(car proto))))
 
-(defop GVAR  3
-  (lambda (vm)
-    (vm:push! vm (vm:gref vm (vm:gvarname (vm:instruction vm))))))
+(defop  1 (LVAR frame var) (vm:push! vm (vm:lvar-ref vm frame var)))
+(defop  2 (LSET frame var) (vm:lvar-set! vm frame var (vm:top vm)))
+(defop  3 (GVAR vname) (vm:push! vm (vm:gref vm vname)))
+(defop  4 (GSET vname) (vm:gset! vm vname (vm:top vm)))
+(defop  5 (POP) (vm:pop! vm))
+(defop  6 (CONST c) (vm:push! vm c))
+(defop  7 (JUMP dest) (vm:set-pc! vm dest))
+(defop  8 (FJUMP dest) (if (false? (vm:pop! vm)) (vm:set-pc! vm dest)))
+(defop  9 (TJUMP dest) (if (true? (vm:pop! vm)) (vm:set-pc! vm dest)))
+(defop 10 (SAVE dest) (vm:push! vm (vm:make-return-address pc: dest fn: (vm:fun vm) env: (vm:env vm))))
 
-(defop GSET  4
-  (lambda (vm)
-    (vm:gset! vm (vm:gvarname (vm:instruction vm)) (vm:top vm))))
+(defop 11 (RETURN) ;; return value is top of stack; return-address is second
+  (begin 
+    (vm:swap! vm) ;; swap so return address is on top
+    (vm:set-fun! vm (vm:return-address-function (vm:top vm)))
+    (vm:set-code! vm (vm:function-code (vm:fun vm)))
+    (vm:set-env! vm (vm:return-address-environment (vm:top vm)))
+    (vm:set-pc! vm (vm:return-address-pc (vm:top vm)))
+    ;; discard return address
+    (vm:pop! vm)))
 
-(defop POP   5 (lambda (vm)(vm:pop! vm)))
+(defop 12 (CALLJ argcount) 
+  (begin
+    (vm:set-env! vm (cdr (vm:env vm)))                 ; discard the top frame
+    (vm:set-fun! vm (vm:pop vm))
+    (vm:set-code! vm (vm:function-code (vm:fun vm)))
+    (vm:set-env! vm (vm:function-environment (vm:fun vm)))
+    (vm:set-pc! vm 0)
+    (vm:set-argcount! vm argcount)))
 
-(defop CONST 6 (lambda (vm) (vm:push! vm (vm:arg1 (vm:instruction vm)))))
+(defop 13 (ARGS nargs) 
+  (let ((argcount (vm:argcount vm)))
+    (if (= argcount nargs)
+        (let loop ((i 0)(args '()))
+          (if (>= i nargs)
+              (vm:set-env! vm (vm:add-frame (vm:env vm) (vm:make-frame args)))
+              (loop (+ i 1)(cons (vm:pop vm) args))))
+        (error (string-append "Wrong number of arguments; " 
+                              (object->string nargs) " expected; "
+                              (object->string argcount) " supplied.")))))
+
+(defop 14 (ARGS. n-args) 
+  (let ((argcount (vm:argcount vm)))
+    (if (>= argcount n-args)
+        (let loop ((i 0) (args '()) (rest '()))
+          (if (>= i argcount)
+              (vm:set-env! vm (vm:add-frame (vm:env vm) (vm:make-frame (append args (list rest)))))
+              (if (>= i (- argcount n-args))
+                  (loop (+ i 1)
+                        args
+                        (cons (vm:pop vm) rest))
+                  (loop (+ i 1)
+                        (cons (vm:pop vm) args)
+                        rest))))
+        (error (string-append "Wrong number of arguments; " 
+                              (object->string n-args) " or more expected; "
+                              (object->string argcount) " supplied.")))))
+
+(defop 15 (FN fun)
+  (vm:push! vm (vm:make-function code: (vm:function-code fun)
+                                 env: (vm:env vm))))
+
+(defop 16 (PRIM op)
+  (let ((n-args (vm:argcount vm)))
+    (let loop ((i 0)
+               (args '()))
+      (if (>= i n-args)
+          (vm:push! vm (apply op args))
+          (loop (+ i 1)(cons (vm:pop vm) args))))))
+
+(defop 17 (TRUE)(vm:push! vm #t))
+(defop 18 (FALSE)(vm:push! vm #f))
+(defop 19 (MINUSONE)(vm:push! vm -1))
+(defop 20 (ZERO)(vm:push! vm 0))
+(defop 21 (ONE)(vm:push! vm 1))
+(defop 22 (TWO)(vm:push! vm 2))
+(defop 23 (HALT)(vm:set-halted! vm #t))
+
+;;; ---------------------------------------------------------------------
+;;; displaying the vm
+;;; ---------------------------------------------------------------------
+
+(define (%instruction->string instr)
+  (if instr
+      (object->string (cons (vm:opname (opcode instr))
+                            (cdr (vector->list instr))))
+      ""))
+
+(define (vm:show-instruction vm)
+  (let* ((instr (vm:instruction vm)))
+    (newline)
+    (display "  instr: ")
+    (display (%instruction->string instr))))
+
+(define (vm:show-code vm)
+  (let ((code (vm:code vm)))
+    (newline)
+    (display "   code: ")
+    (let loop ((instructions (vector->list code)))
+      (if (null? instructions)
+          (newline)
+          (begin
+            (display (%instruction->string (car instructions)))
+            (newline)
+            (display "         ")
+            (loop (cdr instructions)))))))
+
+(define (vm:show-pc vm)
+  (let* ((pc (vm:pc vm)))
+    (newline)
+    (display "  pc: ")
+    (display (object->string pc))))
+
+(define (%stack->string s)
+  (object->string s))
+
+(define (vm:show-stack vm)
+  (let* ((stack (vm:stack vm)))
+    (newline)
+    (display "  stack: ")
+    (display (%stack->string stack))))
+
+(define (vm:show-vm vm)
+  (newline)
+  (display " halted: ")
+  (display (object->string (vm:halted? vm)))
+  (vm:show-instruction vm)
+  (vm:show-pc vm)
+  (vm:show-stack vm)
+  (vm:show-code vm))
 
 ;;; ---------------------------------------------------------------------
 ;;; running the vm
 ;;; ---------------------------------------------------------------------
+
+(define-macro (vm:prog . instrs)
+  (if (null? instrs)
+      '(list)
+      `(cons (list ,@(car instrs)) (vm:prog . ,(cdr instrs)))))
 
 (define (vm:step vm)
   (vm:set-instruction! vm (vector-ref (vm:code vm) (vm:pc vm)))
   (vm:set-pc! vm (+ 1 (vm:pc vm)))
   (let ((operation (vm:op (opcode (vm:instruction vm)))))
     (operation vm)))
+
+(define (vm:step-show vm)
+  (display "step:")
+  (vm:show-pc vm)
+  (vm:show-stack vm)
+  (vm:set-instruction! vm (vector-ref (vm:code vm) (vm:pc vm)))
+  (vm:show-instruction vm)
+  (vm:set-pc! vm (+ 1 (vm:pc vm)))
+  (let ((operation (vm:op (opcode (vm:instruction vm)))))
+    (operation vm))
+  (newline)
+  (newline))
+
+(define (vm:run vm)
+  (let loop ()
+    (if (vm:halted? vm)
+        (if (null? (vm:stack vm))
+            '()
+            (vm:top vm))
+        (begin
+          (vm:step vm)
+          (loop)))))
+
+(define (vm:run-show vm)
+  (newline)
+  (display "start")
+  (vm:show-vm vm)
+  (let loop ()
+    (if (vm:halted? vm)
+        (begin
+          (display "stop")
+          (vm:show-vm vm)
+          (if (null? (vm:stack vm))
+              '()
+              (vm:top vm)))
+        (begin
+          (vm:step-show vm)
+          (loop)))))
+
+(define (vm:run-program code #!key (show #f))
+  (let* ((f (vm:make-function code: code env: '()))
+         (m (vm:make-vm fun: f)))
+    (if show
+        (vm:run-show m)
+        (vm:run m))
+    (if (null? (vm:stack m))
+        #!void
+        (vm:top m))))
+
+#| tests
+
+(vm:run-program (vm:as-code `((,HALT))) show: #t)
+(vm:run-program (vm:as-code `((,CONST 5)(,HALT))) show: #t)
+(vm:run-program (vm:as-code `((,CONST 5)(,GSET x)(,POP)(,HALT))) show: #t)
+(vm:run-program (vm:as-code `((,CONST 5)(,GSET x)(,POP)(,GVAR x)(,HALT))) show: #t)
+
+
+|#
 
