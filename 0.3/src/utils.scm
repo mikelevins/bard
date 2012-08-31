f2740de46e8e9cd3d2c939b316c2cc0dff02cdcf
mikel evins <mevins@me.com>
2012-08-30 20:01:41 -0500

    restarting 0.3

--- 0.3/src/utils.scm
+++
@@ -1,9 +0,0 @@
-(define (vector-position-if pred vec)
-  (let ((len (vector-length vec)))
-    (let loop ((i 0))
-      (if (< i len)
-          (let ((elt (vector-ref vec i)))
-            (if (pred elt)
-                i
-                (loop (+ 1 i))))
-          #f))))
