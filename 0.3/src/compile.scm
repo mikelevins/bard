1d33377b0941deb73cfdd9d562449f14c9e48440
mikel evins <mevins@me.com>
2012-11-03 15:19:32 -0500

    tidied up the top-level compiler logic

--- 0.3/src/compile.scm
+++ 0.3/src/compile.scm
@@ -9,5 +9,38 @@
 ;;;;
 ;;;; ***********************************************************************
 
+
+
+(define (%get-compiler-for stype)
+  (case stype
+    ((syntax:app) %compile-app)
+    ((syntax:character) %compile-self-evaluating)
+    ((syntax:empty-app) %compile-self-evaluating)
+    ((syntax:empty-list) %compile-self-evaluating)
+    ((syntax:empty-string) %compile-self-evaluating)
+    ((syntax:empty-table) %compile-self-evaluating)
+    ((syntax:false) %compile-self-evaluating)
+    ((syntax:keyword) %compile-self-evaluating)
+    ((syntax:lambda-list) %compile-method-lambda-list)
+    ((syntax:list) %compile-self-evaluating)
+    ((syntax:method) %compile-method)
+    ((syntax:nothing) %compile-self-evaluating)
+    ((syntax:number) %compile-self-evaluating)
+    ((syntax:self-evaluating) %compile-self-evaluating)
+    ((syntax:series) %compile-series-expression)
+    ((syntax:symbol) %compile-variable-reference)
+    ((syntax:table) %compile-self-evaluating)
+    ((syntax:table) %compile-self-evaluating)
+    ((syntax:text) %compile-self-evaluating)
+    ((syntax:true) %compile-self-evaluating)
+    ((syntax:undefined) %compile-self-evaluating)
+    (else #f)))
+
+(define (%syntax-type expr)
+  (car expr))
+
 (define (%compile expr env)
-  )
+  (let ((_compile (%get-compiler-for (%syntax-type expr)))) 
+    (if _compile
+        (_compile expr env)
+        (error (str "Syntax error: " expr)))))
