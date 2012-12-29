9d1ffd5c49de2845cfe5637cc05e0287c9e3a868
mikel evins <mevins@me.com>
2012-12-29 02:28:05 -0600

    working on a new implementation of types+dispatch; golas are speedups and support for taxonomy and next-method (yep; inheritance is back again)

---
+++ interpreter/src/c3/function.scm
@@ -0,0 +1,14 @@
+;;;; ***********************************************************************
+;;;; FILE IDENTIFICATION
+;;;;
+;;;; Name:          function.scm
+;;;; Project:       Bard
+;;;; Purpose:       bard functions: representation and dispatch
+;;;; Author:        mikel evins
+;;;; Copyright:     2012 by mikel evins
+;;;;
+;;;; ***********************************************************************
+
+(define $bard-type-precedence-lists
+  (make-table test: eqv?))
+
