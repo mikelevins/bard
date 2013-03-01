e0ee1a36f57e1e0546aa2a611ce4e3db314b7f35
mikel evins <mevins@me.com>
2013-03-01 15:20:02 -0600

    fixed up toplevel load a little

--- 0.4/bard.rkt
+++ 0.4/bard.rkt
@@ -1,8 +1,12 @@
 #lang bard
 
+(require "types.rkt")
+(require "values.rkt")
 (require "special.rkt")
 
 ;;; in the repl:
 ;;; (require racket/enter)
 ;;; (enter! 'bard)
+;;; (current-readtable (make-bard-readtable))
+
 
