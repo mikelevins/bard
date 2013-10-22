34295335752920a84a1e9293015cb20d3b2285ac
mikel evins <mevins@me.com>
2013-10-22 10:09:35 -0500

    added the character reader

--- 0.4/lisp/reader.lisp
+++ 0.4/lisp/reader.lisp
@@ -20,19 +20,51 @@
     (setf (reader:readtable-case tbl) :preserve)
     tbl))
 
+(defparameter *character-name-table* (make-hash-table :test 'equalp))
+
+(defmethod define-character-name ((cid string)(ch cl:character))
+  (setf (gethash cid *character-name-table*) ch))
+
+(defmethod find-character ((cid string))
+  (gethash cid *character-name-table* nil))
+
+(progn
+    (define-character-name "space" #\space)
+    (define-character-name "tab" #\tab)
+    (define-character-name "return" #\return)
+    (define-character-name "newline" #\newline))
+
 ;;; ---------------------------------------------------------------------
 ;;; dispatch-macro characters
 ;;; ---------------------------------------------------------------------
 
 (reader:set-dispatch-macro-character #\# #\d
-  ;; In both Common Lisp and Bard,
-  ;; #x, #o and #b are hexidecimal, octal, and binary,
-  ;; e.g. #xff = #o377 = #b11111111 = 255
-  ;; In Bard only, #d255 is decimal 255.
-  #'(lambda (stream &rest ignore)
-      (declare (ignore ignore))
-      (let ((reader:*read-base* 10)) (bard-read stream)))
-  *bard-readtable*)
+                                     ;; In both Common Lisp and Bard,
+                                     ;; #x, #o and #b are hexidecimal, octal, and binary,
+                                     ;; e.g. #xff = #o377 = #b11111111 = 255
+                                     ;; In Bard only, #d255 is decimal 255.
+                                     #'(lambda (stream &rest ignore)
+                                         (declare (ignore ignore))
+                                         (let ((reader:*read-base* 10)) (bard-read stream)))
+                                     *bard-readtable*)
+
+(reader:set-macro-character #\\ 
+                            (lambda (stream char)
+                              (let* ((char-sym (reader:read stream))
+                                     (digits '(#\0 #\1 #\2 #\3 #\4 #\5 #\6 #\7 #\8 #\9)))
+                                (cond
+                                  ((eof? char-sym) (error "Unexpected end of input while reading a character"))
+                                  ((and (typep char-sym 'cl:number)
+                                        (<= 0 char-sym 9))
+                                   (elt digits char-sym))
+                                  ((typep char-sym 'cl:symbol)
+                                   (let ((char-name (cl:symbol-name char-sym)))
+                                     (if (= 1 (length char-name))
+                                         (character (elt char-name 0))
+                                         (or (find-character char-name)
+                                             (error "Invalid character syntax: ~x~S" char char-sym)))))
+                                  (t (error "Invalid character syntax: ~S" char-sym)))))
+                            nil *bard-readtable*)
 
 ;;; ---------------------------------------------------------------------
 ;;; bard reader support
@@ -75,7 +107,7 @@
                          (sym (intern sname *package*)))
                     (setf (get sym 'bard::module) (intern mname (find-package :bard-modules)))
                     (accept 'symbol sym)))))
-        ;; no colon in token, let's just intern the symbol in the current package:
+        ;; no colon in token; intern it in the current module
         (let ((sym (intern (token-text token) *package*)))
           (setf (get sym 'bard::module) (bard::current-module-name))
           (accept 'symbol sym)))))
@@ -90,4 +122,5 @@
 (defun bard-read-from-string (s)
   (with-input-from-string (in s)
     (bard-read in)))
+
 
