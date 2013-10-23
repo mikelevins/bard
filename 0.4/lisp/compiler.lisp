60da60ef64fe989d9ffbf7fe50e3ddd385f67317
mikel evins <mevins@me.com>
2013-10-23 18:56:17 -0500

    renamed values source files to make them obvious; added source files for the compiler and vm

---
+++ 0.4/lisp/compiler.lisp
@@ -0,0 +1,13 @@
+;;;; ***********************************************************************
+;;;; FILE IDENTIFICATION
+;;;;
+;;;; Name:          compiler.lisp
+;;;; Project:       Bard
+;;;; Purpose:       the bard compiler
+;;;; Author:        mikel evins, after Norvig
+;;;;
+;;;; ***********************************************************************
+
+(in-package :bard)
+
+
---
+++ 0.4/lisp/vm.lisp
@@ -0,0 +1,14 @@
+;;;; ***********************************************************************
+;;;; FILE IDENTIFICATION
+;;;;
+;;;; Name:          vm.lisp
+;;;; Project:       Bard
+;;;; Purpose:       the bard vm
+;;;; Author:        mikel evins, after Norvig
+;;;;
+;;;; ***********************************************************************
+
+(in-package :bard)
+
+
+
