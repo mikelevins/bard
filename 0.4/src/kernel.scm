90b354891b9b4811784a84d82a13fd80f9107457
mikel evins <mevins@me.com>
2015-01-22 21:46:48 -0600

    wrote eval-application

--- 0.4/src/kernel.scm
+++ 0.4/src/kernel.scm
@@ -26,8 +26,45 @@
          (body (cons 'begin (drop 1 form))))
     (call/cc (lambda (k)(kernel:eval body (env:add-binding env exit-var k))))))
 
+(define (kernel:eval-function-application expr env)
+  (not-yet-implemented 'kernel:eval-function-application))
+
+(define (kernel:eval-lambda expr env)
+  (not-yet-implemented 'kernel:eval-lambda))
+
+(define (kernel:eval-begin expr env)
+  (not-yet-implemented 'kernel:eval-begin))
+
+(define (kernel:eval-cond expr env)
+  (not-yet-implemented 'kernel:eval-cond))
+
+(define (kernel:eval-define expr env)
+  (not-yet-implemented 'kernel:eval-define))
+
+(define (kernel:eval-ensure expr env)
+  (not-yet-implemented 'kernel:eval-ensure))
+
+(define (kernel:eval-if expr env)
+  (not-yet-implemented 'kernel:eval-if))
+
+(define (kernel:eval-quote expr env)
+  (not-yet-implemented 'kernel:eval-quote))
+
+(define (kernel:eval-set! expr env)
+  (not-yet-implemented 'kernel:eval-set!))
+
 (define (kernel:eval-application expr env)
-  (not-yet-implemented 'kernel:eval-application))
+  (case (first expr)
+    ((^)(kernel:eval-lambda expr env))
+    ((begin)(kernel:eval-begin expr env))
+    ((cond)(kernel:eval-cond expr env))
+    ((define)(kernel:eval-define expr env))
+    ((ensure)(kernel:eval-ensure expr env))
+    ((if)(kernel:eval-if expr env))
+    ((quote)(kernel:eval-quote expr env))
+    ((set!)(kernel:eval-set! expr env))
+    ((with-exit)(kernel:eval-with-exit expr env))
+    (else (kernel:eval-function-application expr env))))
 
 (define (kernel:eval-constant expr)
   expr)
@@ -39,3 +76,4 @@
                      (kernel:eval-with-exit expr env)
                      (kernel:eval-application expr env)))
    (else (kernel:eval-constant expr))))
+
