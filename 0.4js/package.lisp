c3f6cdf0adb2bbb77f77d276e064bb3265f3d412
mikel evins <mevins@me.com>
2015-02-13 21:32:29 -0600

    tinkering with a js kernel for bard

---
+++ 0.4js/package.lisp
@@ -0,0 +1,14 @@
+;;;; ***********************************************************************
+;;;;
+;;;; Name:          package.lisp
+;;;; Project:       bardvm
+;;;; Purpose:       package definitions
+;;;; Author:        mikel evins
+;;;; Copyright:     2015 by mikel evins
+;;;;
+;;;; ***********************************************************************
+
+(defpackage #:bardvm
+  (:use #:cl #:parenscript))
+
+
