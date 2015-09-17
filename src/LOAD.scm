4ac9d8ac8bd7181e773fce3e8c80ca7ffe3aae1e
mikel evins <mevins@me.com>
2015-09-17 16:22:24 -0500

    a version of bard running which is essentially just Scheme

---
+++ src/LOAD.scm
@@ -0,0 +1,18 @@
+;;;; ***********************************************************************
+;;;;
+;;;; Name:          LOAD.scm
+;;;; Project:       bard 0.4
+;;;; Purpose:       system loader
+;;;; Author:        mikel evins
+;;;; Copyright:     2014 by mikel evins
+;;;;
+;;;; ***********************************************************************
+
+(require 'list-lib)
+(require 'srfi-95) ; sorting
+
+(require language)
+(require repl)
+
+;;; (bard-repl)
+
