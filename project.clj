2ba975955fb2f99b1e0ff47981f32f11201b0037
mikel evins <mevins@me.com>
2016-03-06 19:35:23 -0600

    fiddling with project dependencies

--- project.clj
+++ project.clj
@@ -8,9 +8,9 @@
   
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "1.7.170"]
-                 [org.clojure/core.async "0.2.374"
-                  :exclusions [org.clojure/tools.reader]]
-                 [reagent "0.5.1"]]
+                 [org.clojure/core.async "0.2.374" :exclusions [org.clojure/tools.reader]]
+                 [reagent "0.5.1"]
+                 ]
   
   :plugins [[lein-figwheel "0.5.0-6"]
             [lein-cljsbuild "1.1.2" :exclusions [[org.clojure/clojure]]]]
