589d715731eefaa6cf4b5cdf3986e951fcc1c42e
mikel evins <mevins@me.com>
2016-05-31 04:48:08 -0500

    a bunch of work on datatype infrastructure

---
+++ src/singleton-structures.scm
@@ -0,0 +1,17 @@
+;;;; ***********************************************************************
+;;;;
+;;;; Name:          singleton-structures.scm
+;;;; Project:       Bard
+;;;; Purpose:       user-definable singletons
+;;;; Author:        mikel evins
+;;;; Copyright:     2016 by mikel evins
+;;;;
+;;;; ***********************************************************************
+
+(declare (extended-bindings))
+
+;;; ---------------------------------------------------------------------
+;;; singletons - individual values treated as types for dispatch
+;;; ---------------------------------------------------------------------
+
+
