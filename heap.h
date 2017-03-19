449f33f773e45cc8997d45bae7d5b6e34d312d2a
mikel evins <mevins@me.com>
2017-03-19 12:50:10 -0500

    added notes about heap plans

--- heap.h
+++ heap.h
@@ -1,3 +1,11 @@
+// ======================================================================
+// heap
+// ======================================================================
+// for now the heap is a single array of cells.  later we will
+// implement garbage collection by dynamically allocating heaps and
+// copying live values into them as-needed, recycling old heaps as we
+// go
+
 #define HEAP_SIZE 262144 // number of available cells in a heap
 
 struct cell heap[HEAP_SIZE];
