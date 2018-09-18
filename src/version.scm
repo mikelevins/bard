10bae45599197773a437097bdd985002af842880
mikel evins <mevins@me.com>
2018-09-17 20:25:16 -0500

    updated version string

--- src/version.scm
+++ src/version.scm
@@ -1,14 +1,13 @@
 ;;;; ***********************************************************************
-;;;; FILE IDENTIFICATION
 ;;;;
 ;;;; Name:          version.scm
 ;;;; Project:       Bard
 ;;;; Purpose:       the Bard interpreter version string
 ;;;; Author:        mikel evins
-;;;; Copyright:     2012 by mikel evins
+;;;; Copyright:     2018 by mikel evins
 ;;;;
 ;;;; ***********************************************************************
 
 
-(define $bard-version-string "Bard version 0.3.9")
+(define $bard-version-string "Bard version 0.4.0d1")
 
