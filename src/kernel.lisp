40c7111c4ae2936ede76382d0e1fafd61170698b
mikel evins <mevins@me.com>
2019-12-28 19:59:52 -0600

    added kernel.lisp

---
+++ src/kernel.lisp
@@ -0,0 +1,7 @@
+;;;; kernel.lisp
+
+(in-package #:bard.internal)
+
+;;; the internal Lisp operators that implement the kernel language
+;;; that is the target of the bard compiler in compiler.lisp
+
