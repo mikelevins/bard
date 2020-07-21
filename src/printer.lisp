b23101b705d554c0c1429507bee499d38641e3fd
mikel evins <mevins@me.com>
2020-07-21 18:34:48 -0500

    added a printer for fset:seq values

---
+++ src/printer.lisp
@@ -0,0 +1,14 @@
+;;;; -*- Mode: Lisp; Syntax: Common-Lisp -*-
+;;;; ---------------------------------------------------------------------
+;;;; bardvm
+;;;; A VM implementation based on Norvig's Scheme compiler from PAIP
+;;;; printer.lisp
+;;;; custome bard printers
+;;;; ---------------------------------------------------------------------
+;;;; Code from Paradigms of Artificial Intelligence Programming
+;;;; Copyright (c) 1991 Peter Norvig
+;;;; integrated into bardvm by mikel evins, mikel@evins.net, July 2020
+
+(in-package :bardvm)
+
+
