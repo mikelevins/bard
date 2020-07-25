db0cf624fd874862ec1d6924a7b55c1034713086
mikel evins <mevins@me.com>
2020-07-25 10:52:01 -0500

    moved the main loop into a method on an instance of a vm class; made it execute by calling a step function, so that we can single-step execution of the vm and examine vm registers

--- src/vm.lisp
+++ src/vm.lisp
@@ -23,111 +23,6 @@
   "Compile and execute the expression."
   (machine (compiler `(exit ,exp))))
 
-(defun machine (f)
-  "Run the abstract machine on the code for f."
-  (let* ((code (method-code f))
-         (pc 0)
-         (env nil)
-         (stack nil)
-         (n-args 0)
-         (instr nil))
-    (loop
-       (setf instr (elt code pc))
-       (incf pc)
-       (case (opcode instr)
-
-         ;; Variable/stack manipulation instructions:
-         (LVAR   (push (elt (elt env (arg1 instr)) (arg2 instr))
-                       stack))
-         (LSET   (setf (elt (elt env (arg1 instr)) (arg2 instr))
-                       (top stack)))
-         (GVAR   (push (get (arg1 instr) 'global-val) stack))
-         (GSET   (setf (get (arg1 instr) 'global-val) (top stack)))
-         (POP    (pop stack))
-         (CONST  (push (arg1 instr) stack))
-
-         ;; Branching instructions:
-         (JUMP   (setf pc (arg1 instr)))
-         (FJUMP  (if (null (pop stack)) (setf pc (arg1 instr))))
-         (TJUMP  (if (pop stack) (setf pc (arg1 instr))))
-
-         ;; Function call/return instructions:
-         (SAVE   (push (make-ret-addr :pc (arg1 instr)
-                                      :method f :env env)
-                       stack))
-         (RETURN ;; return value is top of stack; ret-addr is second
-          (setf f (ret-addr-method (second stack))
-                code (method-code f)
-                env (ret-addr-env (second stack))
-                pc (ret-addr-pc (second stack)))
-          ;; Get rid of the ret-addr, but keep the value
-          (setf stack (cons (first stack) (rest2 stack))))
-         (CALLJ  (pop env)                 ; discard the top frame
-                 (setf f  (pop stack)
-                       code (method-code f)
-                       env (method-env f)
-                       pc 0
-                       n-args (arg1 instr)))
-         (ARGS   (assert (= n-args (arg1 instr)) ()
-                         "Wrong number of arguments:~
-                         ~d expected, ~d supplied"
-                         (arg1 instr) n-args)
-                 (push (make-array (arg1 instr)) env)
-                 (loop for i from (- n-args 1) downto 0 do
-                       (setf (elt (first env) i) (pop stack))))
-         (ARGS.  (assert (>= n-args (arg1 instr)) ()
-                         "Wrong number of arguments:~
-                         ~d or more expected, ~d supplied"
-                         (arg1 instr) n-args)
-                 (push (make-array (+ 1 (arg1 instr)) :initial-element nil) env)
-                 (loop repeat (- n-args (arg1 instr)) do
-                       (push (pop stack) (elt (first env) (arg1 instr))))
-                 (loop for i from (- (arg1 instr) 1) downto 0 do
-                       (setf (elt (first env) i) (pop stack))))
-         (METHOD     (push (make-method :code (method-code (arg1 instr))
-                                :env env) stack))
-         (PRIM   (push (apply (arg1 instr)
-                              (loop with args = nil repeat n-args
-                                    do (push (pop stack) args)
-                                    finally (return args)))
-                       stack))
-
-         ;; Continuation instructions:
-         (SET-CC (setf stack (top stack)))
-         (CC     (push (make-method
-                         :env (list (vector stack))
-                         :code '((ARGS 1) (LVAR 1 0 ";" stack) (SET-CC)
-                                 (LVAR 0 0) (RETURN)))
-                       stack))
-
-         ;; Nullary operations:
-         ((%%READ %%NEWLINE) ; *** fix, gat, 11/9/92
-          (push (funcall (opcode instr)) stack))
-
-         ;; Unary operations:
-         ((%%WRITE CAR CDR CADR COMPILER %%DISPLAY %%FIRST %%LIST1 NOT RANDOM %%REST)
-          (push (funcall (opcode instr) (pop stack)) stack))
-
-         ;; Binary operations:
-         ((+ - * / < > <= >= /= = CONS %%LIST2 NAME! EQ EQUAL EQL)
-          (setf stack (cons (funcall (opcode instr) (second stack)
-                                     (first stack))
-                            (rest2 stack))))
-
-         ;; Ternary operations:
-         (%%LIST3
-          (setf stack (cons (funcall (opcode instr) (third stack)
-                                     (second stack) (first stack))
-                            (rest3 stack))))
-
-         ;; Constants:
-         ((T NIL -1 0 1 2)
-          (push (opcode instr) stack))
-
-         ;; Other:
-         ((HALT) (RETURN (top stack)))
-         (otherwise (error "Unknown opcode: ~a" instr))))))
-
 (defclass vm ()
   ((method :accessor vm-method :initform nil )
    (code :accessor vm-code :initform nil )
@@ -243,3 +138,10 @@
       (progn (warn "No method loaded")
              vm)))
 
+
+(defmethod machine ((m method))
+  (let ((vm (make-instance 'vm)))
+    (setf (vm-method vm) m)
+    (setf (vm-code vm) (method-code m))
+    (setf (vm-pc vm) 0)
+    (run-vm vm)))
