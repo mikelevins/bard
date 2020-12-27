c3c3a26e2481b678314a60e1b7de372f5ba732fe
mikel evins <mikel@evins.net>
2020-12-26 20:32:51 -0600

    bumped the version number

--- src/version.scm
+++ src/version.scm
@@ -10,5 +10,5 @@
 ;;;; ***********************************************************************
 
 
-(define $bard-version-string "Bard version 0.7.1")
+(define $bard-version-string "Bard version 0.7.2")
 
