369865f3645e85814061dffa502337e5ee59d54e
mikel evins <mevins@me.com>
2024-09-01 13:48:30 -0500

    setting up the reader and printer

---
+++ src/printer.lisp
@@ -0,0 +1,11 @@
+;;;; ***********************************************************************
+;;;;
+;;;; Name:          printer.lisp
+;;;; Project:       the bard programming language
+;;;; Purpose:       definition of the bard printer
+;;;; Author:        mikel evins
+;;;; Copyright:     2024 by mikel evins
+;;;;
+;;;; ***********************************************************************
+
+(in-package :bard.internal)
