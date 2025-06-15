f2ea89961b62011cbd713eea93ed3558ec66c30b
mikel evins <mevins@me.com>
2025-06-15 13:51:18 -0500

    implemented enough instructions to execute and time a loop

--- src/vm.lisp
+++ src/vm.lisp
@@ -13,7 +13,11 @@
 ;;; conditions
 
 (define-condition exitvm (condition)
-  ((vm :initarg vm :reader vm)))
+  ((vm :initarg :vm :reader vm)))
+
+(define-condition undefined-vm-global (condition)
+  ((vm :initarg vm :reader vm)
+   (varname :initarg :varname :reader varname)))
 
 ;;; Instructions
 
@@ -36,32 +40,37 @@
           (defmethod bytecode->opcode ((b (eql ,bytecode))) ',name)))
 
 (defbytecode HALT 0) ; (bytecode->opcode HALT)
-(defbytecode VNIL 1) 
-(defbytecode VT 2) 
-(defbytecode LVAR 3) 
-(defbytecode LSET 4)
-(defbytecode GVAR 5)
-(defbytecode GSET 6)
-(defbytecode VPOP 7)
-(defbytecode CONST 8)
-(defbytecode JUMP 9)
-(defbytecode FJUMP 10)
-(defbytecode TJUMP 11)
-(defbytecode VSAVE 12)
-(defbytecode VRET 13)
-(defbytecode CALL 14)
-(defbytecode ARGS 15)
-(defbytecode ARGS& 16)
-(defbytecode FN 17)
-(defbytecode PRIM 18)
-(defbytecode SETCC 19)
-(defbytecode CC 20)
-(defbytecode -ONE 21)
-(defbytecode ZERO 22)
-(defbytecode ONE 23)
-(defbytecode TWO 22)
-(defbytecode ADD 23)
-(defbytecode SUB 24)
+(defbytecode VPRINTLN 1) 
+(defbytecode VNIL 2) 
+(defbytecode VT 3) 
+(defbytecode LREF 4) 
+(defbytecode LSET 5)
+(defbytecode GREF 6)
+(defbytecode GSET 7)
+(defbytecode VPOP 8)
+(defbytecode CONST 9)
+(defbytecode JUMP 10)
+(defbytecode FJUMP 11)
+(defbytecode TJUMP 12)
+(defbytecode VSAVE 13)
+(defbytecode VRET 14)
+(defbytecode CALL 15)
+(defbytecode ARGS 16)
+(defbytecode ARGS& 17)
+(defbytecode FN 18)
+(defbytecode PRIM 19)
+(defbytecode SETCC 20)
+(defbytecode CC 21)
+(defbytecode -ONE 22)
+(defbytecode ZERO 23)
+(defbytecode ONE 24)
+(defbytecode TWO 25)
+(defbytecode INC1 26)
+(defbytecode DEC1 27)
+(defbytecode VEQ 28)
+(defbytecode VEQL 29)
+(defbytecode ADD 30)
+(defbytecode SUB 31)
 
 ;;; functions
 
@@ -79,17 +88,46 @@
 ;;; vm
 
 (defun top (s)(first s))
+(defmethod arg1 ((vm vm))(first (instr-args (vm-instr vm))))
+(defmethod arg2 ((vm vm))(first (instr-args (vm-instr vm))))
+(defmethod arg3 ((vm vm))(first (instr-args (vm-instr vm))))
+(defmethod args ((vm vm))(instr-args (vm-instr vm)))
 
 (defclass vm ()
   ((code :initform nil :accessor vm-code)
    (pc :accessor vm-pc :initform 0)
+   (globals :accessor vm-globals :initform (make-hash-table))
    (env :accessor vm-env :initform nil)
    (stack :accessor vm-stack :initform nil)
    (nargs :accessor vm-nargs :initform 0)
    (instr :accessor vm-instr :initform nil)))
 
+
+(defmethod vm-show-globals ((vm vm) &key (stream *standard-output*))
+  (let ((globals (vm-globals vm)))
+    (format stream "~%globals:")
+    (loop for k being the hash-keys in globals using (hash-value v)
+          do (format stream "~%  ~S ~S" k v))))
+
 (defmethod vmreset ((vm vm))
-  (setf (vm-pc vm) 0))
+  (setf (vm-pc vm) 0)
+  (setf (vm-globals vm) (make-hash-table))
+  (setf (vm-env vm) nil)
+  (setf (vm-stack vm) nil)
+  (setf (vm-nargs vm) 0)
+  (setf (vm-instr vm) nil))
+
+(defmethod vm-gref ((vm vm)(var symbol))
+  (let ((found (gethash var (vm-globals vm) :global-absent)))
+    (if (eq found :global-absent)
+        (signal 'undefined-vm-global :varname var :vm vm)
+        found)))
+
+#+repl (vm-gref $vm 'x)
+
+(defmethod vm-gset! ((vm vm)(var symbol) val)
+  (setf (gethash var (vm-globals vm))
+        val))
 
 (defmethod vmload ((vm vm)(code vector))
   (setf (vm-code vm) code)
@@ -102,10 +140,18 @@
            (let ((bc (instr-bytecode instr)))
              ;; machine control
              (cond ((eql HALT bc)(signal 'exitvm)))
+             (cond ((eql VPOP bc)(pop stack)))
+             (cond ((eql VPRINTLN bc)(format t "~%~S" (pop stack))))
+             ;; globals
+             (cond ((eql GREF bc)(push (vm-gref vm (pop stack)) stack)))
+             (cond ((eql GSET bc)(let ((args nil))
+                                   (push (pop stack) args)
+                                   (push (pop stack) args)
+                                   (apply 'vm-gset! vm args))))
              ;; branching
-             (cond ((eql JUMP bc)(setf pc (first (instr-args instr)))))
-             (cond ((eql FJUMP bc)(when (null (pop stack))(setf pc (first (instr-args instr))))))
-             (cond ((eql TJUMP bc)(when (pop stack)(setf pc (first (instr-args instr))))))
+             (cond ((eql JUMP bc)(setf pc (arg1 vm))))
+             (cond ((eql FJUMP bc)(when (null (pop stack))(setf pc (arg1 vm)))))
+             (cond ((eql TJUMP bc)(when (pop stack)(setf pc (arg1 vm)))))
              ;; constant ops
              (cond ((eql VNIL bc)(push nil stack)))             
              (cond ((eql VT bc)(push t stack)))             
@@ -114,9 +160,20 @@
              (cond ((eql ONE bc)(push 1 stack)))             
              (cond ((eql TWO bc)(push 2 stack)))             
              ;; unary ops
-             (cond ((eql CONST bc)(push (first (instr-args instr))
-                                        stack)))
+             (cond ((eql CONST bc)(push (arg1 vm) stack)))
+             (cond ((eql INC1 bc)(push (1+ (pop stack)) stack)))
+             (cond ((eql DEC1 bc)(push (1- (pop stack)) stack)))
              ;; binary ops
+             (cond ((eql VEQ bc)(let ((args nil))
+                                  (push (pop stack) args)
+                                  (push (pop stack) args)
+                                  (push (apply 'eq args)
+                                        stack))))
+             (cond ((eql VEQL bc)(let ((args nil))
+                                   (push (pop stack) args)
+                                   (push (pop stack) args)
+                                   (push (apply 'eql args)
+                                         stack))))
              (cond ((eql ADD bc)(let ((args nil))
                                   (push (pop stack) args)
                                   (push (pop stack) args)
@@ -128,6 +185,14 @@
                                   (push (apply '- args)
                                         stack))))))))
 
+(defmethod stepshowvm ((vm vm))
+  (with-slots (code pc env stack nargs instr) vm
+    (if (< pc (length code))
+        (progn (format t "~%~S" (elt code pc))
+           (stepvm vm)
+           (describe vm))
+        (format t "~%Done."))))
+
 (defmethod runvm ((vm vm))
   (with-slots (code pc env stack nargs instr) vm
     (handler-case (loop (stepvm vm))
@@ -135,8 +200,10 @@
 
 #+repl (defparameter $vm (make-instance 'vm))
 #+repl (describe $vm)
-#+repl (runvm $vm)
 #+repl (stepvm $vm)
+#+repl (stepshowvm $vm)
+#+repl (vm-show-globals $vm)
+#+repl (runvm $vm)
 #+repl (vmreset $vm)
 #+repl (vmload $vm (vector (instr HALT)))
 #+repl (vmload $vm (vector (instr CONST 5)(instr HALT)))
@@ -145,3 +212,26 @@
 #+repl (vmload $vm (vector (instr CONST 2)(instr CONST 3)(instr ADD)(instr HALT)))
 #+repl (vmload $vm (vector (instr CONST 5)(instr CONST 2)(instr SUB)(instr HALT)))
 #+repl (vmload $vm (vector (instr VNIL)(instr FJUMP 3)(instr CONST 101)(instr HALT)))
+#+repl (vmload $vm (vector (instr CONST 'X)   ; 0
+                           (instr CONST 10000)  ; 1
+                           (instr GSET)       ; 2
+                           (instr CONST 'X)   ; 3
+                           (instr GREF)       ; 4
+                           (instr ZERO)       ; 5
+                           (instr VEQL)       ; 6
+                           (instr TJUMP 18)   ; 7
+                           (instr CONST 'X)   ; 8
+                           (instr GREF)       ; 9                           
+                           (instr VPRINTLN)   ; 10                           
+                           (instr VPOP)       ; 11
+                           (instr CONST 'X)   ; 12
+                           (instr CONST 'X)   ; 13
+                           (instr GREF)       ; 14
+                           (instr DEC1)       ; 15
+                           (instr GSET)       ; 16
+                           (instr JUMP 3)     ; 17
+                           (instr HALT)))
+
+#+repl (defparameter $vm (make-instance 'vm))
+#+repl (time (runvm $vm))
+#+repl (time (loop for i from 10000 above 0 do (format t "~%~S" i)))
